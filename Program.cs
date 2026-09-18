7c63d9fa84f60c0fd5900652f0d161967de6f047
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-09-18 21:54:09 +0400

    Feat:extracting first and second numbers from query

--- Program.cs
+++ Program.cs
@@ -3,5 +3,10 @@ var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
     
+    string? firstNumber = context.Request.Query["firstNumber"];
+    string? secondNumber = context.Request.Query["secondNumber"];
+    string? operation = context.Request.Query["operation"];
+    switch
+    await context.Response.WriteAsync($"<p></p>");
 });
 app.Run();
