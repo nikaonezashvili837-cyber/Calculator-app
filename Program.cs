6a9394894d4b426058dcbed12b696998601d3db8
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-09-19 15:54:54 +0400

    Feat:implementing feature which receives two numbers and performes operations on them

--- Program.cs
+++ Program.cs
@@ -2,11 +2,50 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
-    
-    string? firstNumber = context.Request.Query["firstNumber"];
-    string? secondNumber = context.Request.Query["secondNumber"];
-    string? operation = context.Request.Query["operation"];
-    switch
-    await context.Response.WriteAsync($"<p></p>");
+    string path = context.Request.Path;
+    if (path == "/" && context.Request.Query.Count > 0)
+
+    {
+        try
+        {
+            int firstNumber;
+            int secondNumber;
+            Int32.TryParse(context.Request.Query["firstNumber"],out firstNumber);
+            Int32.TryParse(context.Request.Query["secondNumber"],out secondNumber);
+            string? operation = context.Request.Query["operation"];
+            string result;
+            switch (operation)
+            {
+                case "add":
+                    result = $"{firstNumber + secondNumber}";
+                    break;
+                case "subtract":
+                    result = $"{firstNumber - secondNumber}";
+                    break;
+                case "multiply":
+                    result = $"{firstNumber * secondNumber}";
+                    break;
+                case "divide":
+                    result = $"{((decimal)firstNumber / secondNumber)}";
+                    break;
+                default:
+                    context.Response.StatusCode = 400;
+                    result = "Invalid input for 'operation";
+                    break;
+            }
+            await context.Response.WriteAsync($"<p>{result}<p>");
+        }
+        catch
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("<h1>Error</h1>");
+        }
+        ;
+
+    }
+    else if (path == "/")
+    {
+        await context.Response.WriteAsync("<h1>Welcome to calculator app</h1>");
+    }
 });
 app.Run();
